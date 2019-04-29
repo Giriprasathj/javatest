d729e1fc72c2fffc82f7ef0a1600807447cf9173
Trainee GSS <trainee@kgisl.com>
2019-04-29 14:31:40 +0530

    git commit -m "First commit"

---
+++ junit/src/main/java/com/kgisl/junit/Write.java
@@ -0,0 +1,8 @@
+package com.kgisl.junit;
+
+class write {
+public static void main(String[] args) {
+    try(FileWriter fileWriter = new FileWriter("C:\\Users\\trainee.KGISL\\Desktop\\READ.txt");
+    
+}
+}
